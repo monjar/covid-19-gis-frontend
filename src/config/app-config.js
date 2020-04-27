18f3c6d21e44c19ef83f99c224cb96e5f8357377
Amirali Monjar <monjaramirali@gmail.com>
2020-04-27 05:45:18 +0430

    error handling added.

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,9 +1,12 @@
 import countriesController from "../controllers/CountriesController.js";
+import errorController from "../controllers/ErrorController.js";
 import express from "express";
 const configApp = (app) => {
   app.use(express.static("public"));
   app.set("view engine", "ejs");
   app.use("/", countriesController);
+
+  app.use("*", errorController);
 };
 
 export default configApp;
--- src/services/CountriesService.js
+++ src/services/CountriesService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { cleanCountriesData } from "../utils/DataHandler.js";
-const apiURL = "http://covid19api.xapix.io/v2/locations";
+const apiURL = "http://covid19api.xapix.io/v2/locationss";
 
 const getCountriesList = (passDataToView, passErrorView) => {
   axios
